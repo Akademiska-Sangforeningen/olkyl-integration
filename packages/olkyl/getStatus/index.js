2e193cb2275bb982948bc599aca9c2dd1c7af4d4
Rasmus Järnström <rasmus.jarnstrom@columbiaroad.com>
2023-01-12 18:29:38 +0200

    Add status on on/off

--- packages/olkyl/getStatus/index.js
+++ packages/olkyl/getStatus/index.js
@@ -9,8 +9,8 @@ async function main(args) {
   const shellyDeviceId = process.env["DEVICE_ID"];
   const shellyKey = process.env["SHELLY_KEY"];
 
-  // if (args.token !== token)
-  //   return { statusCode: 401, body: { text: "Invalid token" } };
+  if (args.token !== token)
+    return { statusCode: 401, body: { text: "Invalid token" } };
 
   if (args.text === "på" || args.text === "av") {
     const controlUrl = `${shellyUrl}/device/relay/control`;
@@ -31,7 +31,6 @@ async function main(args) {
 
     const formBody = formBodyParts.join("&");
 
-    console.log(formBody);
     const status = await (
       await fetch(controlUrl, {
         method: "POST",
@@ -41,31 +40,37 @@ async function main(args) {
         body: formBody,
       })
     ).json();
-    if (status.isok)
-      return { body: { text: "ok", response_type: "in_channel" } };
+
+    if (status.isok) {
+      const text = await new Promise((resolve) => setTimeout(() => resolve(getStatusText()), 1000))
+      return { body: { text, response_type: "in_channel" } };
+    }
     else return { body: { text: "misslyckades" } };
   } else {
-    const statusUrl = `${shellyUrl}/device/status?id=${shellyDeviceId}&auth_key=${shellyKey}`;
-    const status = await (await fetch(statusUrl)).json();
-
-    const isOn = status?.data?.device_status?.relays[0]?.ison;
-    const power = status?.data?.device_status?.meters[0]?.power;
-
-    if (status?.data.online)
-      return {
-        body: {
-          text: `Ölkylen är ${isOn ? "på" : "av"} och drar ${power}W`,
-          response_type: "in_channel",
-        },
-      };
-    else
-      return {
-        body: {
-          text: "Ölkylen är inte kopplad",
-          status,
-        },
-      };
+    return {
+      body: {
+        text: await getStatusText(),
+        response_type: "in_channel",
+      },
+    };
   }
+}
+
+
+const getStatusText = async () => {
+  const token = process.env["SLACK_TOKEN"];
+  const shellyUrl = process.env["SHELLY_URL"];
+  const shellyDeviceId = process.env["DEVICE_ID"];
+  const shellyKey = process.env["SHELLY_KEY"];
+
+  const statusUrl = `${shellyUrl}/device/status?id=${shellyDeviceId}&auth_key=${shellyKey}`;
+  const status = await (await fetch(statusUrl)).json();
+
+  const isOn = status?.data?.device_status?.relays[0]?.ison;
+  const power = status?.data?.device_status?.meters[0]?.power;
+
+  if (status?.data.online) return `Ölkylen är ${isOn ? `PÅ och drar ${power}W` : "av"}`
+  else return  "Ölkylen är inte kopplad"
 }
 
 // IMPORTANT: Makes the function available as a module in the project.
